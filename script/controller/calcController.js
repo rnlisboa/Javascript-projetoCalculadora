09791f5289be72a9f54ebd61f0681544ce9f0838
ren4nl1sb <renanlisboa443@gmail.com>
2022-08-14 21:52:44 -0300

    Aplicando mais mudanças

--- script/controller/calcController.js
+++ script/controller/calcController.js
@@ -13,6 +13,7 @@ class calcController{
         this.calculo();
         this.clear();
         this._varSupport = ''
+        this._varSupport2 = ''
         this._displayCalc = [];
     }
     initialize(){
@@ -22,7 +23,7 @@ class calcController{
         }, 1000)
     }
     display(){
-        //this._showDisplay.innerHTML = 'hello'
+        //this._showDisplay.innerHTML = this._varSupport
     }
     
     buttonEvents(){
@@ -37,54 +38,53 @@ class calcController{
         let btn8 = document.querySelector('.btn-8')
         let btn9 = document.querySelector('.btn-9')
         btn0.addEventListener('click', ()=>{
-            //this._displayCalc.push('0')
             this._varSupport += '0'
-            console.log(this._varSupport)
+            this._varSupport2 = this._varSupport
             this._showDisplay.innerHTML = this._varSupport
         })
         btn1.addEventListener('click', ()=>{
             this._varSupport += '1'
-            console.log(this._varSupport)
+            this._varSupport2 = this._varSupport
             this._showDisplay.innerHTML = this._varSupport
         })
         btn2.addEventListener('click', ()=>{
             this._varSupport += '2'
-            console.log(this._varSupport)
+            this._varSupport2 = this._varSupport
             this._showDisplay.innerHTML = this._varSupport
         })
         btn3.addEventListener('click', ()=>{
             this._varSupport += '3'
-            console.log(this._varSupport)
+            this._varSupport2 = this._varSupport
             this._showDisplay.innerHTML = this._varSupport
         })
         btn4.addEventListener('click', ()=>{
             this._varSupport += '4'
-            console.log(this._varSupport)
+            this._varSupport2 = this._varSupport
             this._showDisplay.innerHTML = this._varSupport
         })
         btn5.addEventListener('click', ()=>{
             this._varSupport += '5'
-            console.log(this._varSupport)
+            this._varSupport2 = this._varSupport
             this._showDisplay.innerHTML = this._varSupport
         })
         btn6.addEventListener('click', ()=>{
             this._varSupport += '6'
-            console.log(this._varSupport)
+            this._varSupport2 = this._varSupport
             this._showDisplay.innerHTML = this._varSupport
         })
         btn7.addEventListener('click', ()=>{
             this._varSupport += '7'
-            console.log(this._varSupport)
+            this._varSupport2 = this._varSupport
             this._showDisplay.innerHTML = this._varSupport
         })
         btn8.addEventListener('click', ()=>{
             this._varSupport += '8'
-            console.log(this._varSupport)
+            this._varSupport2 = this._varSupport
             this._showDisplay.innerHTML = this._varSupport
         })
         btn9.addEventListener('click', ()=>{
             this._varSupport += '9'
-            console.log(this._varSupport)
+            this._varSupport2 = this._varSupport
             this._showDisplay.innerHTML = this._varSupport
         })
 
@@ -93,54 +93,59 @@ class calcController{
     operations(){
         let soma = document.querySelector('.btn-soma')
         soma.addEventListener('click', () =>{
-            this._displayCalc.push(this._varSupport)
-            this._varSupport = ''
-            this._displayCalc.push('+')
-            console.log(this._displayCalc)
-            
+            this._varSupport += '+'
+            this._showDisplay.innerHTML = this._varSupport
         })
         let subt = document.querySelector('.btn-subtracao')
         subt.addEventListener('click', () =>{
-            this._displayCalc.push(this._varSupport)
-            this._varSupport = ''
-            this._displayCalc.push('-')
-            console.log(this._displayCalc)
-            
+            this._varSupport += '-'
+            this._showDisplay.innerHTML = this._varSupport
         })
         let divs = document.querySelector('.btn-divisao')
         divs.addEventListener('click', () =>{
-            this._displayCalc.push(this._varSupport)
-            this._varSupport = ''
-            this._displayCalc.push('/')
-            console.log(this._displayCalc)
-            
+            this._varSupport += '/'
+            this._showDisplay.innerHTML = this._varSupport
         })
         let multp = document.querySelector('.btn-multiplicacao')
         multp.addEventListener('click', () =>{
-            this._displayCalc.push(this._varSupport)
-            this._varSupport = ''
-            this._displayCalc.push('*')
-            console.log(this._displayCalc)
-            
-            
+            this._varSupport += '*'
+            this._showDisplay.innerHTML = this._varSupport
+        })
+        
+        let prcnt = document.querySelector('.btn-percent')
+        prcnt.addEventListener('click', () =>{
+            this._showDisplay.innerHTML = ''
+        })
+        let root = document.querySelector('.btn-root')
+        root.addEventListener('click', () =>{
+            this._varSupport = this._varSupport2 + '** (1 / 2)'
+            this._showDisplay.innerHTML = '√ ' + this._varSupport2
+        })
+        let sqr = document.querySelector('.btn-inverse')
+        sqr.addEventListener('click', () =>{
+            this._varSupport = '1 / ' + this._varSupport2
+            this._showDisplay.innerHTML = this._varSupport2
+        })
+        let inverse = document.querySelector('.btn-square')
+        inverse.addEventListener('click', () =>{
+            this._varSupport = this._varSupport2 + '** 2'
+            this._showDisplay.innerHTML = this._varSupport2 + '²'
         })
     }
     calculo(){
         
         let igual = document.querySelector('.btn-igual') 
         igual.addEventListener('click', ()=>{
-            let lastItem = this._displayCalc[this._displayCalc.length - 1 ]
-            if (this._displayCalc.length >= 4){
-                if (lastItem === '+' ||  lastItem === '-' || lastItem === '*' || lastItem === '/'){
-                    this._displayCalc.pop()
-                }
-                this._displayCalc.push(this._varSupport)
-                let string = this._displayCalc.join(' ')
-                let stringToCalc = eval(string)
-                console.log(stringToCalc)
-                this._showDisplay.innerHTML = stringToCalc
-                this._displayCalc = []
-            }
+            
+           let resultado = eval(this._varSupport)
+           let result = resultado.toString()
+           if(result.length > 13){
+                this._showDisplay.innerHTML = result.substr(0,6)
+           }
+           else{
+            this._showDisplay.innerHTML = resultado
+           }
+                
             
         })  
     }
